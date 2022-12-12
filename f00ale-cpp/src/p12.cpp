bf661c998e2a2e05114a6b8b440739593c399a03
Arno Lepisk <arno@lepisk.se>
2022-12-12 19:03:22 +0100

    Day 12 speedup.

--- f00ale-cpp/src/p12.cpp
+++ f00ale-cpp/src/p12.cpp
@@ -7,8 +7,8 @@
 #include <deque>
 
 std::tuple<std::string, std::string> p12(const std::string &input) {
-    int64_t ans1 = 0;
-    int64_t ans2 = 0;
+    int ans1 = 0;
+    int ans2 = 0;
 
     std::vector<std::vector<int>> v;
 
@@ -46,43 +46,36 @@ std::tuple<std::string, std::string> p12(const std::string &input) {
 
     }
 
-    for(auto p : {1,2}){
-        if(p == 2) {
-            startx = endx;
-            starty = endy;
-        }
-
-        std::deque<std::tuple<int, int, int>> q{{startx, starty, 0}};
-        std::set<std::tuple<int, int>> visited{{startx, starty}};
-        auto go = [p, &q, &visited, &v](int curr, int nx, int ny, int s) {
-            if (nx >= 0 && nx < static_cast<int>(v[0].size()) && ny >= 0 && ny < static_cast<int>(v.size())) {
-                if (visited.find({nx, ny}) == visited.end()) {
-                    if ((p == 1 && v[ny][nx] <= curr + 1) || (p == 2 && v[ny][nx] >= curr - 1)) {
-                        visited.insert({nx, ny});
-                        q.emplace_back(nx, ny, s);
-                    }
+    std::deque<std::tuple<int, int, int>> q{{endx, endy, 0}};
+    std::vector<std::vector<char>> visited(v.size(), std::vector<char>(v[0].size(), 0));
+    visited[endy][endx] = 1;
+    auto go = [&q, &visited, &v](int curr, int nx, int ny, int s) {
+        if (nx >= 0 && nx < static_cast<int>(v[0].size()) && ny >= 0 && ny < static_cast<int>(v.size())) {
+            if (!visited[ny][nx]) {
+                if (v[ny][nx] >= curr - 1) {
+                    visited[ny][nx] = 1;
+                    q.emplace_back(nx, ny, s);
                 }
             }
-        };
-
-        while (!q.empty()) {
-            auto [x, y, s] = q.front();
-            q.pop_front();
-            auto curr = v[y][x];
-            if (p == 1 && x == endx && y == endy) {
-                ans1 = s;
-                break;
-            } else if(p == 2 && curr == 0) {
-                ans2 = s;
-                break;
-            }
-
-            go(curr, x + 1, y, s + 1);
-            go(curr, x - 1, y, s + 1);
-            go(curr, x, y + 1, s + 1);
-            go(curr, x, y - 1, s + 1);
-
         }
+    };
+
+    while (!q.empty()) {
+        auto [x, y, s] = q.front();
+        q.pop_front();
+        auto curr = v[y][x];
+        if(curr == 0 && !ans2) {
+            ans2 = s;
+        }
+        if (x == startx && y == starty) {
+            ans1 = s;
+            break;
+        }
+
+        go(curr, x + 1, y, s + 1);
+        go(curr, x - 1, y, s + 1);
+        go(curr, x, y + 1, s + 1);
+        go(curr, x, y - 1, s + 1);
     }
 
     return {std::to_string(ans1), std::to_string(ans2)};
